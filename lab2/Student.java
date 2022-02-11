0e4746578a7c8dd75e680a1de73ebba2a559342a
Krishnamoorti Bhat <69892786+KMmoodgar@users.noreply.github.com>
2022-02-11 22:36:13 +0530

    Add files via upload

---
+++ lab2/Student.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class Student
+{
+	String usn,name,course;
+
+	public Student(String usn, String name, String course) 
+	{
+		this.usn = usn;
+		this.name = name;
+		this.course = course;
+	}
+	
+}
---
+++ lab2/arraylist.java
@@ -0,0 +1,61 @@
+package classes;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class arraylist
+{
+	public static void main(String[] args)
+	{
+		ArrayList<Student> array = new ArrayList<Student>();
+		Scanner sc = new Scanner(System.in);
+		
+		while(true)
+		{
+			System.out.println("1.ADD\n2.REMOVE\n3.DISPLAY\n4.SIZE\n5.ISEMPTY\n6.EXIT");
+			System.out.println("Enter your choice : ");
+			int ch = sc.nextInt();
+			switch(ch)
+			{
+			case 1:
+				System.out.println("Enter student usn :");
+				String usn = sc.next();
+				System.out.println("Enter student name :");
+				String name = sc.next();
+				System.out.println("Enter student course : ");
+				String course = sc.next();
+				Student s = new Student(usn,name,course);
+				array.add(s);
+				break;
+			case 2:
+				 System.out.println("Enter the index numebr to delete :");
+				 int index = sc.nextInt();
+				 if(index < array.size())
+				 {
+					 array.remove(index);
+				 }
+				 else
+				 {
+					 System.out.println("Array index out of bound");
+				 }
+				 break;
+			case 3:
+				for(Student stud: array)
+				{
+					System.out.println(stud.name);
+				}
+				break;
+			case 4:
+				System.out.println(array.size());
+				break;
+			case 5:
+				System.out.println(array.isEmpty());
+				break;
+			case 6:
+				System.exit(0);
+				break;
+				
+			}
+		}
+	}
+}
